f9b3d77a7b96ac8f84abb842bfc814397e541057
antoniosilva376 <pg47033@alunos.uminho.pt>
2023-05-22 22:37:30 +0100

    write result of api request to csv

--- tp.py
+++ tp.py
@@ -1,4 +1,5 @@
 import spotipy
+import csv
 from spotipy import oauth2
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -6,7 +7,7 @@ from tokens import client_token, secret_token
 import spotipy.util as util
 import json
 
-# gets all songs from the playlists, grouped by owner of playlist
+
 def getSongsFromPlaylist(spotify, list_of_playlists):
 
     res = {}
@@ -35,14 +36,24 @@ def getSongsFromPlaylist(spotify, list_of_playlists):
     return res
 
 def main():
+
+    # initiate spotify client
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyClientCredentials(client_id=client_token, client_secret=secret_token))
 
+    # input here the playlist ids
     list_of_playlists = ['3YjyB3anOIsK7qw8cdUeV4','0wSuXtjnRL4Gzyqjbi39jd']
 
+    # result from all the playlists
     res = getSongsFromPlaylist(spotify, list_of_playlists)
-    print(res)
 
+    # writing to csv
+    with open('output.csv', 'w', newline='') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        for value in res.values():
+            writer.writerow(value)
+
+    # call here fptree method
 
 
 if __name__ == '__main__':
