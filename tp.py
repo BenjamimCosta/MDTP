599cd73baa266f22d059ddea67aa3309514d9916
antoniosilva376 <pg47033@alunos.uminho.pt>
2023-05-22 23:21:05 +0100

    fptree algorithm added

--- tp.py
+++ tp.py
@@ -1,15 +1,11 @@
 import spotipy
 import csv
-from spotipy import oauth2
-from spotipy.oauth2 import SpotifyOAuth
+from fpgrowth_py import fpgrowth
 from spotipy.oauth2 import SpotifyClientCredentials
 from tokens import client_token, secret_token
-import spotipy.util as util
-import json
 
 
 def getSongsFromPlaylist(spotify, list_of_playlists):
-
     res = {}
 
     for playlist in list_of_playlists:
@@ -30,19 +26,20 @@ def getSongsFromPlaylist(spotify, list_of_playlists):
 
         for item in results['tracks']['items']:
             song_id = item['track']['id']
-            #res[owner_id][playlist].append(song_id)
+            # res[owner_id][playlist].append(song_id)
             res[playlist].append(song_id)
 
     return res
 
-def main():
 
+def main():
     # initiate spotify client
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyClientCredentials(client_id=client_token, client_secret=secret_token))
 
     # input here the playlist ids
-    list_of_playlists = ['3YjyB3anOIsK7qw8cdUeV4','0wSuXtjnRL4Gzyqjbi39jd']
+    # list_of_playlists = ['3YjyB3anOIsK7qw8cdUeV4','0wSuXtjnRL4Gzyqjbi39jd']
+    list_of_playlists = ['0hO6tRnbeCUU6IGjlMxcut', '2CuIyKHyAKKqA9ruLOOUop', '53Ykpk4dim5I6LAMG7Ayk8']
 
     # result from all the playlists
     res = getSongsFromPlaylist(spotify, list_of_playlists)
@@ -53,9 +50,18 @@ def main():
         for value in res.values():
             writer.writerow(value)
 
-    # call here fptree method
+    itemSetList = []
+
+    # getting from csv (will be useful if we dont use api in the future)
+    with open('output.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            itemSetList.append(row)
+
+    freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=0.5, minConf=0.5)
+    print(freqItemSet)
+    print(rules)
 
 
 if __name__ == '__main__':
     main()
-
